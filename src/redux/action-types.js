331fd68511d311371afe030b0bddd409a67989b5
surbhi <surbhisaga337865@gmail.com>
2022-05-31 16:57:41 +0530

    couter using react redux-saga

--- src/redux/action-types.js
+++
@@ -1,2 +0,0 @@
-export const INCREAMENT = "INCREAMENT";
-export const DECREMENT = "DECREMENT";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
-import { rootReducer } from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import couterReducer from "./feactures/couterSlice";
 
-export const store = createStore(
-  rootReducer,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default configureStore({
+  reducer: {
+    counter: couterReducer,
+  },
+});
